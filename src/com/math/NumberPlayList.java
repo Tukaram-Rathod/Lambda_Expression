462117bef08a80ee68d734061a99c7c0bc5a73b4
Tukaram-Rathod <tukaram.p.rathod02@gmail.com>
2021-04-20 17:36:29 +0530

    Added.Created a Number Play List and iterate and print each elements

---
+++ src/com/math/NumberPlayList.java
@@ -0,0 +1,50 @@
+package com.math;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class NumberPlayList {
+    public static void main(String[] args) {
+        //creating sample collection
+        List<Integer> myNumberList = new ArrayList<Integer>();
+        for (int i=0; i<5;i++) myNumberList.add(i);
+
+        //Method 1: traversing uding Iterator
+        Iterator<Integer> it = myNumberList.iterator();
+        while (it.hasNext()){
+            Integer i = it.next();
+            System.out.println("Iterator Value:"+i);
+        }
+        //Method 2: Traversing with Consumer interface implementation
+        class MyConsumer implements Consumer<Integer>{
+            public void accept(Integer t){
+                System.out.println("Mth2:Consumer impl Value::"+t );
+            }
+        }
+        MyConsumer action = new MyConsumer();
+        myNumberList.forEach(action);
+
+        //Method 3: traversing with Anonymous Consumer interface implementation
+        myNumberList.forEach(new Consumer<Integer>() {
+            @Override
+            public void accept(Integer t) {
+                System.out.println("Mth3: forEach anonymous class Value::"+t);
+            }
+        });
+
+        //Method 4: Explicit Lambda Function
+        Consumer<Integer>myListAction = n->{
+            System.out.println("Mth4: forEach Lambda impl Value::"+n);
+
+        };
+        myNumberList.forEach(myListAction);
+
+        //method 5 :Implcit Lambda Function
+        myNumberList.forEach (n->{
+            System.out.println("Mth5: forEach Llambda imp Value::"+n);
+        });
+    }
+
+}
